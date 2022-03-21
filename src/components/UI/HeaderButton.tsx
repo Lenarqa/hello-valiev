7776318cef9732b625d402a7a897778e09c4618c
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-03-22 00:50:47 +0700

    create header component, create HeaderButton component, add styles

---
+++ src/components/UI/HeaderButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 17px 28px;
+  background-color: #585cc6;
+  color: #ffffff;
+  font-family: "Gilroy-Regular", sans-serif;
+  font-weight: 600;
+  font-size: 18px;
+`;
+
+const HeaderButton: React.FC = (props) => {
+  return <Button>{props.children}</Button>;
+};
+export default HeaderButton;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import HeaderButton from "../UI/HeaderButton";
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 32px 80px;
+  width: 100%;
+  height: 116px;
+  background-color: #fff;
+  text-align: center;
+`;
+
+const PhotoSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Name = styled.h2`
+    font-family: "Gilroy";
+    color: #333;
+    font-size: 18px;
+    font-family: 'Gilroy-Bold', sans-serif;
+`
+
+const Title = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: #585CC6;
+    font-family: 'Gilroy-Bold', sans-serif;
+
+    p:first-child {
+        font-size: 32px;
+        font-weight: 700;
+        color: #585CC6;
+    }
+    p:last-child {
+        text-transform: uppercase;
+    } 
+`
+
+const Img = styled.img`
+    height: 52px;
+    width: 52px;
+    border-radius: 2px;
+    margin-right: 20px;
+`
+
+const Header: React.FC = () => {
+  return (
+    <StyledHeader>
+      <PhotoSection>
+        <Img src={require("../../assets/img/photo.jpg")} alt="photo" />
+        <Name>Ленар Валиев</Name>
+      </PhotoSection>
+      <Title>
+          <p>ilink</p>
+          <p>academy</p>
+      </Title>
+      <div>
+        <HeaderButton>Панель управления</HeaderButton>
+      </div>
+    </StyledHeader>
+  );
+};
+
+export default Header;
