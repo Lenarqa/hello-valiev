9ebf8e6f76ee2281fb833c26f83fe73de1216a40
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-04-13 01:41:04 +0700

    refactor(AuthModal): add helper functions and replace repeated code with that functions

--- src/components/modal/authModal/AuthModal.tsx
+++ src/components/modal/authModal/AuthModal.tsx
@@ -48,6 +48,12 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
     }
   }, [isEmailError, isPasswordError, email, password]);
 
+  // helper function
+  const setErrorEmail = (errorMsg: string) => {
+    setIsEmailError(true);
+    setEmailErrorMsg(errorMsg);
+  };
+
   const emailValidationHandler = (e: React.FormEvent<HTMLInputElement>) => {
     const emailValidation =
       /^((?=[a-zA-Z0-9])[a-zA-Z0-9!#$%&\\'*+\-\/=?^_`.{|}~]{1,25})@(([a-zA-Z0-9\-]){1,25}\.)([a-zA-Z0-9]{2,4})$/;
@@ -59,20 +65,17 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
     props.hideFooterErrMsg();
 
     if (newValue.trim().length === 0) {
-      setIsEmailError(true);
-      setEmailErrorMsg("Поле не может быть пустым");
+      setErrorEmail("Поле не может быть пустым");
       return;
     }
 
     if (/,/.test(newValue)) {
-      setIsEmailError(true);
-      setEmailErrorMsg("Email не может содержать в себе запятых");
+      setErrorEmail("Email не может содержать в себе запятых");
       return;
     }
 
     if (!/^(?=[a-zA-Z0-9])/.test(newValue)) {
-      setIsEmailError(true);
-      setEmailErrorMsg(
+      setErrorEmail(
         "Email не может начинаться с символа, только с числа или латинских букв, любого регистра"
       );
       return;
@@ -90,16 +93,14 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
     }
 
     if (!newValue.includes("@") && newValue.length > 25) {
-      setIsEmailError(true);
-      setEmailErrorMsg(
+      setErrorEmail(
         "Количество символов до знака @ не может быть длиннее 25 символов"
       );
       return;
     }
 
     if (!/@/.test(newValue)) {
-      setIsEmailError(true);
-      setEmailErrorMsg("Email обязан содержать в себе символ @");
+      setErrorEmail("Email обязан содержать в себе символ @");
       return;
     }
 
@@ -110,8 +111,7 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
       (newValue.includes("@") &&
         newValue.split("@")[1].split(".")[0].length < 1)
     ) {
-      setIsEmailError(true);
-      setEmailErrorMsg(
+      setErrorEmail(
         "Количество символов после знака @ не может быть больше 25 символов и короче 1"
       );
       return;
@@ -125,8 +125,7 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
           newValue.split("@")[1].split(".")[1].length > 4)) ||
       (newValue.includes("@") && !newValue.split("@")[1].includes("."))
     ) {
-      setIsEmailError(true);
-      setEmailErrorMsg(
+      setErrorEmail(
         "Email обязательно должен содержать домен (.ru/com и т.д). Домен не может быть короче 2 символов и длиннее 4"
       );
       return;
@@ -134,18 +133,23 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
 
     //final check, check
     if (!emailValidation.test(newValue)) {
-      setIsEmailError(true);
-      setEmailErrorMsg(
+      setErrorEmail(
         "Email не может включать в себе кирилицу или другие алфавиты, используйте только латиницу"
       );
       return;
     }
 
     // когда пользователь оставляет мышку поверх infoIcon и иконка исчезает
-    // чтобы не оставалось окно без сообщения, когда нет ошибок
+    // чтобы не оставалось окно с описанием ошибки без сообщения, выключаем ховер
     if (emailValidation.test(newValue)) {
       setIsHoverEmail(false);
     }
+  };
+
+  // helper function
+  const setErrorPassword = (errorMsg: string) => {
+    setIsPasswordError(true);
+    setPasswordErrorMsg(errorMsg);
   };
 
   const passwordValidationHandler = (e: React.FormEvent<HTMLInputElement>) => {
@@ -160,50 +164,32 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
     props.hideFooterErrMsg();
 
     if (newValue.trim().length === 0) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg("Поле не может быть пустым");
-      return;
-    }
-
-    if (newValue.trim().length === 0) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg("Поле не может быть пустым");
+      setErrorPassword("Поле не может быть пустым");
       return;
     }
 
     if (!/.*\d/.test(newValue)) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg("Пароль должен содержать цифры");
+      setErrorPassword("Пароль должен содержать цифры");
       return;
     }
 
     if (!/.*[a-z]/.test(newValue)) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg(
-        "Пароль должен содержать латинские буквы нижнего регистра"
-      );
+      setErrorPassword("Пароль должен содержать латинские буквы нижнего регистра");
       return;
     }
 
     if (!/.*[A-Z]/.test(newValue)) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg(
-        "Пароль должен содержать латинские буквы верхнего регистра"
-      );
+      setErrorPassword( "Пароль должен содержать латинские буквы верхнего регистра")
       return;
     }
 
     if (!/.*\W/.test(newValue)) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg("Пароль должен содержать символы (например !№%:?)");
+      setErrorPassword("Пароль должен содержать символы (например !№%:?)");
       return;
     }
 
     if (newValue.length < 8 || newValue.length > 24) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg(
-        "Пароль не может быть короче 8 символов и длиннее 24 символов"
-      );
+      setErrorPassword("Пароль не может быть короче 8 символов и длиннее 24 символов");
       return;
     }
 
@@ -211,19 +197,12 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
     // я не думаю что в пароле должна быть кириллица поэтому добавил проверку
     // если кириллица в пароле все таки имеет место быть я уберу эту проверку.
     if (/[а-яА-ЯёЁ]/.test(newValue)) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg("Пароль может содержать только латинские буквы");
+      setErrorPassword("Пароль не может содержать кириллицу, используйте только латиницу");
       return;
     }
 
-    if (!passwordValidation.test(newValue)) {
-      setIsPasswordError(true);
-      setPasswordErrorMsg("Что то пошло не так");
-      return;
-    }
-
-    // когда пользователь оставляет мышку поверх infoIcon
-    // чтобы не оставалось окно без сообщения, когда нет ошибок
+    // когда пользователь оставляет мышку поверх infoIcon и иконка исчезает
+    // чтобы не оставалось окно с описанием ошибки без сообщения, выключаем ховер
     if (passwordValidation.test(newValue)) {
       setIsHoverPassword(false);
     }
@@ -262,7 +241,7 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
   const passwordRecoveryModalHandler = () => {
     props.hideFooterErrMsg();
     props.showPasswordRecoveryModal();
-  }
+  };
 
   return (
     <form className={style.form} onSubmit={submitHandler}>
@@ -344,4 +323,5 @@ const AuthModal: React.FC<IAuthModal> = (props) => {
     </form>
   );
 };
+
 export default AuthModal;
