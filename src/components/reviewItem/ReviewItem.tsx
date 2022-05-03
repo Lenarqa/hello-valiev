a91946a56667692640c038ed05bbc47e4df481ea
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2022-05-03 13:50:25 +0700

    style(Reviews, ReviewsItem): add adaptive

--- src/components/reviewItem/ReviewItem.tsx
+++ src/components/reviewItem/ReviewItem.tsx
@@ -88,7 +88,6 @@ const ReviewItem: React.FC<ISliderItem> = ({
         className={style.item}
         data-type={type}
         data-is-canceled={isCanceled}
-        // data-is-publish={isPublish}
         data-status={status}
       >
         <div className={style.header}>
